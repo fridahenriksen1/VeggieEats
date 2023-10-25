035fec77ccdca43466296252babe0bb4c384a37b
Frida Henriksen <fridahenriksen@hotmail.com>
2023-10-25 20:34:55 +0200

    added folders

---
+++ abtest/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="sv">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php echo $pagetitel; ?></title>
+    <!-- Preload the largest visible image. -->
+    <!-- <link rel="preload" as="video" fetchpriority="high" href="movie/pasta.mp4"> -->
+
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital@0;1&family=Roboto:wght@300;400;500;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
+
+    
+</head>
+
+<body>
+
+<header class="main-header">
+        <section class="container">
+            <a class="logo" href="index.php">VeggieEats</a>
+            <nav class="main-nav">
+                <ul class="main-menu">
+                    <li class="main-menu-item">
+                        <a class="link" href="recepie.php">Alla recept</a>
+                    </li>
+                    <li class="main-menu-item">
+                        <a class="link" href="blogg.php">Tips</a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+    </header>
