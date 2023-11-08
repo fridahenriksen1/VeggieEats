5e0ded96e4606aded2a9bd98745536248ddc7739
Frida Henriksen <fridahenriksen@hotmail.com>
2023-11-08 13:49:03 +0100

    changes

--- recepie.php
+++ recepie.php
@@ -1,58 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="sv">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recept</title>
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"> -->
-    <!-- Oswald -->
-    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet"> -->
-
-
-
-    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
-
-    <!-- <link
-        href="https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital@0;1&family=Roboto:wght@300;400;500;700&display=swap"
-        rel="stylesheet"> -->
-
-    <!-- ikoner -->
-    <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    rel="stylesheet"> -->
-
-    <!-- <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-
-
-
-
-
-
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <header class="main-header">
-        <section class="container">
-            <a class="logo" href="index.html">VeggieEats</a>
-            <nav class="main-nav">
-                <ul class="main-menu">
-                    <li class="main-menu-item">
-                        <a class="link" href="recepie.html">Alla recept</a>
-                    </li>
-                    <li class="main-menu-item">
-                        <a class="link" href="blogg.html">Tips</a>
-                    </li>
-                </ul>
-            </nav>
-        </section>
-
-    </header> -->
 
     <?php 
     $pagetitel = "VeggieEats";
@@ -121,25 +66,12 @@
             </template>
 
         </section>
-        <!-- <section class="quote">
-            <article class="content">
-                <h3>Lär dig att laga mat</h3>
-                <a class="btn-primary" href="lar-dig-laga-mat.html">klicka här</a>
-                <a href="#" class="btn-secondary">Annars kan du klicka här!</a>
-            </article>
-        </section> -->
+      
     </main>
     <footer id="main-footer">
         <section class="footer-inside">
             <h3>VeggieEats</h3>
-            <!-- <p>Skriv in din mejl här du, du kommer ändå inte få något</p>
-            <form action="/" id="newsletter">
-                <label for="email">
-                    <span class="visually-hidden">Email</span>
-                </label>
-                <input class="textField" type="text" name="email" id="email" placeholder="Email">
-                <input class="btn btn-submit" type="submit" value="Skicka">
-            </form> -->
+          
             <button id="cookie-settings-button" class="cookie-settings-button">Kakinställningar</button>
             <p class="footer-text">©Copyright 2023 VegieEats</p>
         </section>
